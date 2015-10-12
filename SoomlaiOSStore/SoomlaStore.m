c3612a0a3c95ed975685aea638a3c3f6f2644d6a
Boris Spektor <boris@soom.la>
2015-10-12 11:21:02 +0300

    Prepared release of v3.6.15

--- SoomlaiOSStore/SoomlaStore.m
+++ SoomlaiOSStore/SoomlaStore.m
@@ -34,7 +34,7 @@
 
 #import "SoomlaVerification.h"
 
-#define SOOMLA_STORE_VERSION @"3.6.14"
+#define SOOMLA_STORE_VERSION @"3.6.15"
 
 @interface SoomlaStore (){
     NSMutableArray* verifications;
